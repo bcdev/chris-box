1bf78da44b04b19b728c0c2e68e8e40a8cf57d33
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-07-19 17:37:43 +0000

    - changed generated metadata
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@379 d90d2d48-2b33-0410-a973-e72f85796a02

--- chris-noise-reduction/src/main/java/org/esa/beam/chris/operators/DestripingFactorsOp.java
+++ chris-noise-reduction/src/main/java/org/esa/beam/chris/operators/DestripingFactorsOp.java
@@ -140,13 +140,11 @@ public class DestripingFactorsOp extends AbstractOperator {
 
         final StringBuilder sb = new StringBuilder();
         for (int i = 0; i < sourceProducts.length; ++i) {
-            if (i < 0) {
+            if (i > 0) {
                 sb.append(", ");
             }
-            sb.append(sourceProducts[i].getName());
-            sb.append(" [");
             sb.append(getAnnotationString(sourceProducts[i], ChrisConstants.ATTR_NAME_FLY_BY_ZENITH_ANGLE));
-            sb.append("°]");
+            sb.append("°");
         }
         setAnnotationString(targetProduct, ChrisConstants.ATTR_NAME_NR_ACQUISITION_SET, sb.toString());
         // todo -- consider writing the sources metadata to the SPH
