ff0fabf14eafc2a29752489b8e599702c2519356
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-02-15 16:51:12 +0000

    added cloud probability operator
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@1734 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ chris-cloud-screening/src/main/java/org/esa/beam/chris/operators/CloudProbabilityOp.java
@@ -0,0 +1,197 @@
+package org.esa.beam.chris.operators;
+
+import com.bc.ceres.core.ProgressMonitor;
+import com.bc.ceres.core.SubProgressMonitor;
+import org.esa.beam.framework.datamodel.Band;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.framework.gpf.Operator;
+import org.esa.beam.framework.gpf.OperatorException;
+import org.esa.beam.framework.gpf.Tile;
+import org.esa.beam.framework.gpf.OperatorSpi;
+import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
+import org.esa.beam.framework.gpf.annotations.Parameter;
+import org.esa.beam.framework.gpf.annotations.SourceProduct;
+import org.esa.beam.framework.gpf.annotations.TargetProduct;
+import org.esa.beam.util.ProductUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@OperatorMetadata(alias = "chris.ComputeCloudProbability",
+                  version = "1.0",
+                  authors = "Marco Peters, Marco Zühlke",
+                  copyright = "(c) 2008 by Brockmann Consult",
+                  description = "Computes the cloud probability of a pixel.")
+public class CloudProbabilityOp extends Operator {
+    // todo - turn these constants into parameter to generify this operator
+    private static final String BAND_NAME_CLOUD_PROBABILITY = "cloud_probability";
+    private static final String PROBABILITY_PREFIX = "probability_";
+
+
+    @SourceProduct(alias = "cluster")
+    private Product clusterProduct;
+    @SourceProduct(alias = "toaRefl", optional = true)
+    private Product toaReflectanceProduct;
+
+    @TargetProduct(description = "Product containing the accumulated probability.")
+    private Product targetProduct;
+
+    @Parameter(alias = "accumulate", description = "Considered classes which are accumulated to a probability.",
+               notEmpty = true, notNull = true)
+    private int[] accumulateClassIndices;
+    @Parameter(alias = "redistribute", description = "Classes which are redistributed to the other classes.",
+               notEmpty = true, notNull = true)
+    private int[] redistributeClassIndices;
+
+
+    private Map<Integer, Band> probabilityBandMap;
+    private List<Integer> useClassList;
+
+
+    public CloudProbabilityOp() {
+    }
+
+    CloudProbabilityOp(int[] accumulateClassIndices, int[] redistributeClassIndices, Product reflProduct,
+                              Product clusterProduct) {
+        this.accumulateClassIndices = accumulateClassIndices;
+        this.redistributeClassIndices = redistributeClassIndices;
+        this.toaReflectanceProduct = reflProduct;
+        this.clusterProduct = clusterProduct;
+    }
+
+    @Override
+    public void initialize() throws OperatorException {
+
+        validateParameter();
+
+        probabilityBandMap = new HashMap<Integer, Band>(clusterProduct.getNumBands());
+        useClassList = new ArrayList<Integer>(clusterProduct.getNumBands());
+        for (String bandName : clusterProduct.getBandNames()) {
+            if (bandName.startsWith(PROBABILITY_PREFIX)) {
+                final int bandId = Integer.valueOf(bandName.substring(PROBABILITY_PREFIX.length()));
+                probabilityBandMap.put(bandId, clusterProduct.getBand(bandName));
+                if (Arrays.binarySearch(redistributeClassIndices, bandId) < 0) {  // not included
+                    useClassList.add(bandId);
+                }
+            }
+        }
+
+        final Product targetProduct = createTargetProduct();
+
+        setTargetProduct(targetProduct);
+    }
+
+
+    @Override
+    public void computeTile(Band targetBand, Tile targetTile, ProgressMonitor pm) throws OperatorException {
+        try {
+            if (targetBand.getName().startsWith("reflectance_")) {
+                pm.beginTask(String.format("Computing '%s'", targetBand.getName()), 1);
+                final Tile sourceTile = getSourceTile(toaReflectanceProduct.getBand(targetBand.getName()),
+                                                      targetTile.getRectangle(),
+                                                      SubProgressMonitor.create(pm, 1));
+                targetTile.setRawSamples(sourceTile.getRawSamples());
+            } else if (BAND_NAME_CLOUD_PROBABILITY.equals(targetBand.getName())) {
+                pm.beginTask(String.format("Computing '%s'", targetBand.getName()), probabilityBandMap.size() + 1);
+                final Map<Integer, Tile> tileMap = new HashMap<Integer, Tile>(probabilityBandMap.size());
+                for (Map.Entry<Integer,Band> entry : probabilityBandMap.entrySet()) {
+                    final Tile sourceTile = getSourceTile(entry.getValue(), targetTile.getRectangle(),
+                                                          SubProgressMonitor.create(pm, 1));
+                    tileMap.put(entry.getKey(), sourceTile);
+                    if (pm.isCanceled()) {
+                        return;
+                    }
+                }
+                computeCloudProbability(targetTile, tileMap);
+                pm.worked(1);
+            } else {
+                throw new OperatorException(
+                        String.format("Nothing known about a band named '%s'", targetBand.getName()));
+            }
+        } finally {
+            pm.done();
+        }
+    }
+
+    private void validateParameter() {
+        for (int accumulateClass : accumulateClassIndices) {
+            final Band band = clusterProduct.getBand(PROBABILITY_PREFIX + accumulateClass);
+            if (band == null) {
+                throw new OperatorException(
+                        String.format("Not able to find accumulate class with index %d", accumulateClass));
+            }
+        }
+
+        for (int redistributeClass : redistributeClassIndices) {
+            final Band band = clusterProduct.getBand(PROBABILITY_PREFIX + redistributeClass);
+            if (band == null) {
+                throw new OperatorException(
+                        String.format("Not able to find redistribute class with index %d", redistributeClass));
+            }
+        }
+    }
+
+    private Product createTargetProduct() {
+        final String name = clusterProduct.getName().replace("_CLU", "_PROB");
+        final String type = clusterProduct.getProductType().replace("_CLU", "_PROB");
+        final int sceneRasterWidth = clusterProduct.getSceneRasterWidth();
+        final int sceneRasterHeight = clusterProduct.getSceneRasterHeight();
+        final Product targetProduct = new Product(name, type, sceneRasterWidth, sceneRasterHeight);
+        targetProduct.setStartTime(clusterProduct.getStartTime());
+        targetProduct.setEndTime(clusterProduct.getEndTime());
+
+        targetProduct.addBand(BAND_NAME_CLOUD_PROBABILITY, ProductData.TYPE_FLOAT32);
+
+        if (toaReflectanceProduct != null) {
+            if (sceneRasterWidth != toaReflectanceProduct.getSceneRasterWidth() ||
+                sceneRasterHeight != toaReflectanceProduct.getSceneRasterHeight()) {
+                throw new OperatorException("Source products are not spatially equal.");
+            }
+            final Band[] reflBands = toaReflectanceProduct.getBands();
+            for (Band band : reflBands) {
+                if (band.getName().startsWith("reflectance")) {
+                    ProductUtils.copyBand(band.getName(), toaReflectanceProduct, targetProduct);
+                }
+            }
+        }
+        return targetProduct;
+    }
+
+    private void computeCloudProbability(Tile targetTile, Map<Integer, Tile> probabilityTiles) {
+        for (Tile.Pos pos : targetTile) {
+            double accum = accumulateClassProbabilities(probabilityTiles, pos);
+            double sum = sumNotRedistributed(probabilityTiles, pos);
+            targetTile.setSample(pos.x, pos.y, accum / sum);
+        }
+    }
+
+    private double sumNotRedistributed(Map<Integer, Tile> probabilityTiles, Tile.Pos pos) {
+        double accuSum = 0;
+        for (int classIndex : useClassList) {
+            final Tile tile = probabilityTiles.get(classIndex);
+            accuSum = accuSum + tile.getSampleDouble(pos.x, pos.y);
+        }
+        return accuSum;
+    }
+
+    private double accumulateClassProbabilities(Map<Integer, Tile> probabilityTiles, Tile.Pos pos) {
+        double accuSum = 0;
+        for (int classIndex : accumulateClassIndices) {
+            final Tile tile = probabilityTiles.get(classIndex);
+            final double sampleDouble = tile.getSampleDouble(pos.x, pos.y);
+            accuSum = accuSum + sampleDouble;
+        }
+        return accuSum;
+    }
+
+    public static class Spi extends OperatorSpi {
+
+    protected Spi() {
+        super(CloudProbabilityOp.class);
+    }
+}
+}
---
+++ chris-cloud-screening/src/test/java/org/esa/beam/chris/operators/CloudProbabilityOpTest.java
@@ -0,0 +1,191 @@
+package org.esa.beam.chris.operators;
+
+import junit.framework.TestCase;
+import org.esa.beam.framework.datamodel.Band;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.framework.gpf.OperatorException;
+
+import java.awt.Dimension;
+import java.io.IOException;
+
+public class CloudProbabilityOpTest extends TestCase {
+
+    private static final int REFL_BAND_COUNT = 6;
+    private static final Dimension PRODUCT_DIMENSION = new Dimension(2, 2);
+    private static final int CLUSTER_CLASS_BAND_COUNT = 5;
+    private static final double[][] CLUSTER_CLASS_BAND_DATA = new double[][]{
+            {0.10, 0.20, 0.30, 0.40},
+            {0.15, 0.48, 0.12, 0.03},
+            {0.14, 0.10, 0.08, 0.06},
+            {0.20, 0.02, 0.05, 0.01},
+            {0.41, 0.20, 0.45, 0.50},
+    };
+    private static final double[] EXPECTED_PROBABILITY_RESULT = new double[]{
+            0.7183, 0.9523, 0.9375, 0.9890
+    };
+    private static final double PROBABILITY_ACCURACY = 1e-4;
+
+
+    public void testInittializeOK() {
+        Product reflDummy = createToaReflSourceProduct(PRODUCT_DIMENSION, REFL_BAND_COUNT);
+        Product clusterDummy = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(new int[]{0, 4}, new int[]{1, 2},
+                                                                  reflDummy, clusterDummy);
+       probabilityOp.getTargetProduct();
+    }
+
+    public void testInitializeWithoutReflProduct() {
+        Product clusterDummy = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(new int[]{0, 4}, new int[]{1, 2},
+                                                                  null, clusterDummy);
+        probabilityOp.getTargetProduct();
+    }
+
+    public void testSourceProductAreSpatiallyEqual() {
+        Product reflDummy = createToaReflSourceProduct(new Dimension(6, 6), REFL_BAND_COUNT);
+        Product clusterDummy = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(new int[]{0, 4}, new int[]{1, 2},
+                                                                  reflDummy, clusterDummy);
+        try {
+            probabilityOp.getTargetProduct();
+            fail("OperatorException expected: Products are not spatially equal");
+        } catch (OperatorException expected) {
+            // expected
+        }
+    }
+
+    public void testIllegalAccumulateIndex() {
+        // index out of bound
+        Product reflDummy1 = createToaReflSourceProduct(PRODUCT_DIMENSION, REFL_BAND_COUNT);
+        Product clusterDummy1 = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(new int[]{145336}, new int[]{3},
+                                                                  reflDummy1, clusterDummy1);
+        try {
+            probabilityOp.getTargetProduct();
+            fail("OperatorException expected: Accumulate class index is out of bounds");
+        } catch (OperatorException expected) {
+            // expected
+        }
+    }
+
+    public void testIllegalRedistributeIndex() {
+        // index out of bound
+        Product reflDummy = createToaReflSourceProduct(PRODUCT_DIMENSION, REFL_BAND_COUNT);
+        Product clusterDummy = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(new int[]{1, 2}, new int[]{145336},
+                                                                  reflDummy, clusterDummy);
+        try {
+            probabilityOp.getTargetProduct();
+            fail("OperatorException expected: Redistribute class index is out of bounds");
+        } catch (OperatorException expected) {
+            // expected
+        }
+    }
+
+    public void testTargetProduct() {
+        int[] accumulateClassIndices = new int[]{0, 4};
+        int[] redistributeClassIndices = new int[]{1, 2};
+        Product reflDummy = createToaReflSourceProduct(PRODUCT_DIMENSION, REFL_BAND_COUNT);
+        Product clusterDummy = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(accumulateClassIndices, redistributeClassIndices,
+                                                                  reflDummy, clusterDummy);
+
+        final Product targetProduct = probabilityOp.getTargetProduct();
+        assertNull(targetProduct.getGeoCoding());
+        assertEquals(REFL_BAND_COUNT + 1, targetProduct.getNumBands());
+        final Band cloudProbBand = targetProduct.getBand("cloud_probability");
+        assertNotNull(cloudProbBand);
+        assertEquals(ProductData.TYPE_FLOAT32, cloudProbBand.getDataType());
+        for (int i = 1; i <= REFL_BAND_COUNT; i++) {
+            final String bandName = String.format("reflectance_%d", i);
+            assertTrue(String.format("Target product does not contain '%s'", bandName),
+                       targetProduct.containsBand(bandName));
+        }
+    }
+
+    public void testCopyOfToaReflBands() throws IOException {
+        int[] accumulateClassIndices = new int[]{0, 4};
+        int[] redistributeClassIndices = new int[]{1, 2};
+        Product reflSource = createToaReflSourceProduct(PRODUCT_DIMENSION, REFL_BAND_COUNT);
+        Product clusterSource = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(accumulateClassIndices, redistributeClassIndices,
+                                                                  reflSource, clusterSource);
+
+        final Product targetProduct = probabilityOp.getTargetProduct();
+        for (int i = 1; i <= REFL_BAND_COUNT; i++) {
+            final Band targetBand = targetProduct.getBand(String.format("reflectance_%d", i));
+            final Band sourceBand = reflSource.getBand(targetBand.getName());
+            float[] targetSamples = new float[PRODUCT_DIMENSION.width * PRODUCT_DIMENSION.height];
+            targetBand.readPixels(0,0, PRODUCT_DIMENSION.width, PRODUCT_DIMENSION.height, targetSamples);
+
+            for (int j = 0; j < targetSamples.length; j++) {
+                float targetSample = targetSamples[j];
+                final float sourceSample = sourceBand.getRasterData().getElemFloatAt(j);
+                assertEquals(sourceSample, targetSample, 1e-6f);
+            }
+        }
+    }
+
+    public void testCloudProbabilityBand() throws IOException {
+        int[] accumulateClassIndices = new int[]{0, 4};
+        int[] redistributeClassIndices = new int[]{1, 2};
+        Product reflDummy = createToaReflSourceProduct(PRODUCT_DIMENSION, REFL_BAND_COUNT);
+        Product clusterDummy = createClusterSourceProduct(PRODUCT_DIMENSION, CLUSTER_CLASS_BAND_COUNT);
+        CloudProbabilityOp probabilityOp = new CloudProbabilityOp(accumulateClassIndices, redistributeClassIndices,
+                                                                  reflDummy, clusterDummy);
+
+        final Product targetProduct = probabilityOp.getTargetProduct();
+        final Band probabilityBand = targetProduct.getBand("cloud_probability");
+        double[] probalitySamples = new double[PRODUCT_DIMENSION.width * PRODUCT_DIMENSION.height];
+        probabilityBand.readPixels(0,0, PRODUCT_DIMENSION.width, PRODUCT_DIMENSION.height, probalitySamples);
+
+        for (int i = 0; i < EXPECTED_PROBABILITY_RESULT.length; i++) {
+            final double expectedResult = EXPECTED_PROBABILITY_RESULT[i];
+            final double currentResult = probalitySamples[i];
+            assertEquals(expectedResult, currentResult, PROBABILITY_ACCURACY);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // PRIVATE SECTION
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    private Product createClusterSourceProduct(Dimension dim, int numClusterBands) {
+        final Product product = new Product("ChrisM3_CLU", "ChrisM3_CLU", dim.width, dim.height);
+        for (int i = 0; i < numClusterBands; i++) {
+            final Band band = new Band(String.format("probability_%d", i), ProductData.TYPE_FLOAT64,
+                                       dim.width, dim.height);
+            product.addBand(band);
+            final ProductData data = band.createCompatibleRasterData();
+            data.setElems(CLUSTER_CLASS_BAND_DATA[i]);
+            band.setSynthetic(true);
+            band.setRasterData(data);
+        }
+
+        final Band maskBand = product.addBand("membership_mask", ProductData.TYPE_INT8);
+        final ProductData data = maskBand.createCompatibleRasterData();
+        for (int j = 0; j < data.getNumElems(); j++) {
+            data.setElemDoubleAt(j, Math.floor(Math.random() * numClusterBands));
+        }
+        maskBand.setSynthetic(true);
+        maskBand.setRasterData(data);
+        return product;
+    }
+
+    private Product createToaReflSourceProduct(Dimension dim, int numReflBands) {
+        final Product product = new Product("ChrisM3_REFL", "ChrisM3_REFL", dim.width, dim.height);
+        for (int i = 1; i <= numReflBands; i++) {
+            final Band band = product.addBand(String.format("reflectance_%d", i),
+                                              ProductData.TYPE_FLOAT32);
+            final ProductData data = band.createCompatibleRasterData();
+            for (int j = 0; j < data.getNumElems(); j++) {
+                data.setElemDoubleAt(j, Math.random());
+            }
+            band.setSynthetic(true);
+            band.setRasterData(data);
+        }
+        return product;
+    }
+
+}
