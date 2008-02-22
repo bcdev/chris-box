5610b2ba49ada1a4102b7bb98b88deb37b02946b
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-02-22 11:00:16 +0000

    renamings and todos
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@1769 d90d2d48-2b33-0410-a973-e72f85796a02

--- chris-cloud-screening/src/main/java/org/esa/beam/chris/operators/MakeClusterMapOp.java
+++ chris-cloud-screening/src/main/java/org/esa/beam/chris/operators/MakeClusterMapOp.java
@@ -23,13 +23,12 @@ import org.esa.beam.framework.gpf.OperatorException;
 import org.esa.beam.framework.gpf.OperatorSpi;
 import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
 import org.esa.beam.framework.gpf.annotations.SourceProduct;
+import org.esa.beam.framework.gpf.annotations.TargetProduct;
 
 import java.awt.Rectangle;
 import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * todo - API doc
@@ -46,6 +45,8 @@ public class MakeClusterMapOp extends Operator {
 
     @SourceProduct
     private Product sourceProduct;
+    @TargetProduct
+    private Product targetProduct;
 
     @Override
     public void initialize() throws OperatorException {
@@ -55,23 +56,20 @@ public class MakeClusterMapOp extends Operator {
         final String type = sourceProduct.getProductType().replace("_CLU", "_MAP");
         final Product targetProduct = new Product(name, type, width, height);
 
-        final List<Band> bandList = new ArrayList<Band>();
-        int bandId = 0;
-        for (final Band band : sourceProduct.getBands()) {
-            if (band.getName().startsWith("probability")) {
-                bandList.add(band);
-                ProbabilityImageBand probBand = new ProbabilityImageBand(band.getName(), band.getDataType(), width, height, bandId, sourceProduct.getBands());
-                probBand.setDescription("Cluster membership mask");
-                targetProduct.addBand(probBand);
-                probBand.update(new int[]{});
-                bandId++;
-            }
+        final Band[] sourceBands = sourceProduct.getBands();
+        for (int i = 0; i < sourceBands.length; ++i) {
+            final Band sourceBand = sourceBands[i];
+
+            ProbabilityImageBand targetBand = new ProbabilityImageBand(sourceBand.getName(),
+                    sourceBand.getDataType(), width, height, sourceBands, i);
+            targetBand.setDescription(sourceBand.getDescription());
+            targetProduct.addBand(targetBand);
+            targetBand.update(new int[]{});
         }
         final MembershipImageBand membershipBand = new MembershipImageBand("membership_mask", ProductData.TYPE_INT8,
                 width, height, targetProduct.getBands());
         membershipBand.setDescription("Cluster membership mask");
         targetProduct.addBand(membershipBand);
-
         membershipBand.update();
 
         setTargetProduct(targetProduct);
@@ -87,8 +85,7 @@ public class MakeClusterMapOp extends Operator {
         }
 
         public void update() {
-            final RenderedImage image = ClusterMapOpImage.create(this, sourceBands);
-            setImage(image);
+            setImage(ClusterMapOpImage.create(this, sourceBands));
         }
 
         @Override
@@ -103,18 +100,19 @@ public class MakeClusterMapOp extends Operator {
 
     public static class ProbabilityImageBand extends Band {
 
-        private final int bandId;
-        private final Band[] probabilityBands;
+        private final Band[] sourceBands;
+        private final int correspondingBandIndex;
 
-        public ProbabilityImageBand(String name, int dataType, int width, int height, int bandId, Band[] probabilityBands) {
+        public ProbabilityImageBand(String name, int dataType, int width, int height, Band[] sourceBands,
+                                    int correspondingBandIndex) {
             super(name, dataType, width, height);
-            this.bandId = bandId;
-            this.probabilityBands = probabilityBands;
+            this.sourceBands = sourceBands;
+            this.correspondingBandIndex = correspondingBandIndex;
         }
 
-        public void update(int[] redistributeBandIds) {
-            final RenderedImage image = ClusterProbabilityOpImage.create(this, probabilityBands, bandId, redistributeBandIds);
-            setImage(image);
+        // todo - generify or remove parameter in order to make this an interface method
+        public void update(int[] backgroundBandIndexes) {
+            setImage(ClusterProbabilityOpImage.create(this, sourceBands, correspondingBandIndex, backgroundBandIndexes));
         }
 
         @Override
