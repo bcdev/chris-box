4f118fff4eb7c988f91ed30b0b2dbd0700ed528e
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-03-07 14:20:29 +0000

    renamings
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@1866 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ chris-cloud-screening/src/main/java/org/esa/beam/chris/operators/internal/InclusiveMultiBandFilter.java
@@ -0,0 +1,29 @@
+package org.esa.beam.chris.operators.internal;
+
+import org.esa.beam.framework.datamodel.Band;
+
+/**
+ * New class.
+ *
+ * @author Ralf Quast
+ * @version $Revision: 1753 $ $Date$
+ */
+public class InclusiveMultiBandFilter implements BandFilter {
+
+    private final double[][] wavelengthIntervals;
+
+    public InclusiveMultiBandFilter(double[]... wavelengthIntervals) {
+        this.wavelengthIntervals = wavelengthIntervals;
+    }
+
+    public boolean accept(Band band) {
+        final float wavelength = band.getSpectralWavelength();
+
+        for (final double[] interval : wavelengthIntervals) {
+            if (wavelength > interval[0] && wavelength < interval[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
