c1c06d5152d809c305c38a31436ce02420844916
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-04-21 11:00:16 +0000

    aod module renamings
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@1947 d90d2d48-2b33-0410-a973-e72f85796a02

--- chris-aod-retrieval/src/main/resources/doc/help/aodretrieval.hs
+++ chris-aod-retrieval/src/main/resources/doc/help/aodretrieval.hs
@@ -4,7 +4,7 @@
         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-    <title>CHRIS/PROBA Aerosol Optical Depth Calculation Tool Help</title>
+    <title>CHRIS/PROBA Aerosol Optical Depth Retrieval Tool Help</title>
     <maps>
         <homeID>top</homeID>
         <mapref location="map.jhm"/>
