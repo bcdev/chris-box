d08a1ca4503dc2382bc65cb0dd147ed5d9d996a3
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-06-23 11:55:15 +0000

    API doc
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@2307 d90d2d48-2b33-0410-a973-e72f85796a02

--- chris-util/src/main/java/org/esa/beam/chris/operators/BandFilter.java
+++ chris-util/src/main/java/org/esa/beam/chris/operators/BandFilter.java
@@ -3,12 +3,19 @@ package org.esa.beam.chris.operators;
 import org.esa.beam.framework.datamodel.Band;
 
 /**
- * New class.
+ * Band filter interface.
  *
  * @author Ralf Quast
  * @version $Revision: 1864 $ $Date: 2008-03-07 15:16:02 +0100 (Fri, 07 Mar 2008) $
  */
 public interface BandFilter {
 
+    /**
+     * Returns {@code true} if the band supplied as argument has been accepted.
+     *
+     * @param band the band.
+     *
+     * @return {@code true} if the band supplied as argument has been accepted.
+     */
     boolean accept(Band band);
 }
