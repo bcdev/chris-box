b89a625a77a278dcb796b35d5b168dbecc8e430e
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-06-25 16:36:59 +0000

    better tests
    git-svn-id: https://www.brockmann-consult.de/svn/os/chris-box/trunk@5762 d90d2d48-2b33-0410-a973-e72f85796a02

--- chris-geometric-correction/src/main/java/org/esa/beam/chris/operators/ImageCenterTime.java
+++ chris-geometric-correction/src/main/java/org/esa/beam/chris/operators/ImageCenterTime.java
@@ -28,6 +28,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+
 /**
  * Handle image center time reading and calculation
  *
@@ -36,12 +37,82 @@ import java.util.List;
  */
 class ImageCenterTime {
     
+    private static final double DAY_SEC = 86400.0;
+    
     final double ict1;
     final double ict2;
     final double ict3;
     final double ict4;
     final double ict5;
 
+    ImageCenterTime(double ict1, double ict2, double ict3, double ict4, double ict5) {
+        this.ict1 = ict1;
+        this.ict2 = ict2;
+        this.ict3 = ict3;
+        this.ict4 = ict4;
+        this.ict5 = ict5;
+    }
+    
+    static ImageCenterTime create(double[] ictValues, double dTgps) {
+        Calendar calendar = ProductData.UTC.createCalendar();
+        calendar.set(1999, 11, 26, 0, 0);
+        Date jd0Date = calendar.getTime();
+        double jd0 = DateTimeUtils.utcToJD(jd0Date);
+        double ict1 = (ictValues[0] - dTgps) / DAY_SEC + jd0;
+        double ict2 = (ictValues[1] - dTgps) / DAY_SEC + jd0;
+        double ict3 = (ictValues[2] - dTgps) / DAY_SEC + jd0;
+        double ict4 = (ictValues[3] - dTgps) / DAY_SEC + jd0;
+        double ict5 = (ictValues[4] - dTgps) / DAY_SEC + jd0;
+        return new ImageCenterTime(ict1, ict2, ict3, ict4, ict5);
+    }    
+    
+    static class ITCReader {
+        
+        private final String[] lastIctRecord;
+
+        ITCReader(InputStream is) {
+            Reader reader = new InputStreamReader(is);
+            char[] separators = new char[]{'\t'};
+            CsvReader csvReader = new CsvReader(reader, separators, true, "TIME");
+            String[] record = null;
+            try {
+                record = readRecords(csvReader);
+            } catch (IOException e) {
+            }
+            lastIctRecord = record;
+        }
+        
+        private String[] readRecords(CsvReader csvReader) throws IOException {
+            List<String[]> stringRecords = csvReader.readStringRecords();
+            int ndxICT = -1;
+            for (int i = 0; i < stringRecords.size(); i++) {
+                String[] record = stringRecords.get(i);
+                if (record[ICT.ICT1_PKT.index].substring(0, 8).equals("+G:29494")) {
+                    ndxICT = i;
+                }
+            }
+            if (ndxICT != -1) {
+                return stringRecords.get(ndxICT);
+            } else {
+                return null;
+            }
+        }
+        
+        String[] getLastIctRecord() {
+            return lastIctRecord;
+        }
+        
+        double[] getLastIctValues() {
+            double[] ict = new double[5];
+            ict[0] = Double.parseDouble(lastIctRecord[ICT.ICT1.index]);
+            ict[1] = Double.parseDouble(lastIctRecord[ICT.ICT2.index]);
+            ict[2] = Double.parseDouble(lastIctRecord[ICT.ICT3.index]);
+            ict[3] = Double.parseDouble(lastIctRecord[ICT.ICT4.index]);
+            ict[4] = Double.parseDouble(lastIctRecord[ICT.ICT5.index]);
+            return ict;
+        }
+    }
+    
     private enum ICT{
         TIME(0),
         PKT(1),
@@ -63,54 +134,5 @@ class ImageCenterTime {
         private ICT(int index) {
             this.index = index;
         }
-    }
-    
-    private ImageCenterTime(double ict1, double ict2, double ict3, double ict4, double ict5) {
-        this.ict1 = ict1;
-        this.ict2 = ict2;
-        this.ict3 = ict3;
-        this.ict4 = ict4;
-        this.ict5 = ict5;
-    }
-
-    static ImageCenterTime read(InputStream is, double dTgps) {
-        Reader reader = new InputStreamReader(is);
-        char[] separators = new char[]{'\t'};
-        CsvReader csvReader = new CsvReader(reader, separators, true, "TIME");
-        try {
-            List<String[]> stringRecords = csvReader.readStringRecords();
-            int ndxICT = -1;
-            for (int i = 0; i < stringRecords.size(); i++) {
-                String[] record = stringRecords.get(i);
-                if (record[ICT.ICT1_PKT.index].substring(0, 8).equals("+G:29494")) {
-                    ndxICT = i;
-                }
-            }
-            if (ndxICT == -1) {
-                return null;
-            }
-            Calendar calendar = ProductData.UTC.createCalendar();
-            calendar.set(1999, 11, 26, 0, 0);
-            Date jd0Date = calendar.getTime();
-            double jd0 = DateTimeUtils.utcToJD(jd0Date);
-            
-            //Convert from secs to Julian Days
-            String[] record = stringRecords.get(ndxICT);
-
-            final double DAY_SEC = 86400.0;
-            double ict1 = (Double.parseDouble(record[ICT.ICT1.index]) - dTgps) / DAY_SEC + jd0;
-            double ict2 = (Double.parseDouble(record[ICT.ICT2.index]) - dTgps) / DAY_SEC + jd0;
-            double ict3 = (Double.parseDouble(record[ICT.ICT3.index]) - dTgps) / DAY_SEC + jd0;
-            double ict4 = (Double.parseDouble(record[ICT.ICT4.index]) - dTgps) / DAY_SEC + jd0;
-            double ict5 = (Double.parseDouble(record[ICT.ICT5.index]) - dTgps) / DAY_SEC + jd0;
-            return new ImageCenterTime(ict1, ict2, ict3, ict4, ict5);
-        } catch (IOException e) {
-        } finally {
-            try {
-                csvReader.close();
-            } catch (IOException ignored) {
-            }
-        }
-        return null;
     }
 }
--- chris-geometric-correction/src/test/java/org/esa/beam/chris/operators/ImageCenterTimeTest.java
+++ chris-geometric-correction/src/test/java/org/esa/beam/chris/operators/ImageCenterTimeTest.java
@@ -1,5 +1,7 @@
 package org.esa.beam.chris.operators;
 
+import org.esa.beam.chris.operators.ImageCenterTime.ITCReader;
+
 import java.io.InputStream;
 
 import junit.framework.TestCase;
@@ -9,7 +11,26 @@ public class ImageCenterTimeTest extends TestCase {
     public void testReadImageCenterTimes() {
         final InputStream is = ImageCenterTimeTest.class.getResourceAsStream(
                 "Pass2049.Barrax_13350_CHRIS_center_times_20030512_65534");
-        ImageCenterTime imageCenterTime = ImageCenterTime.read(is, 13);
+        
+        ITCReader reader = new ImageCenterTime.ITCReader(is);
+        String[] lastIctRecord = reader.getLastIctRecord();
+        assertEquals("2003.132.11.26.05.341", lastIctRecord[0]);
+        assertEquals("294944", lastIctRecord[1]);
+        assertEquals("+G:294944", lastIctRecord[2]);
+        assertEquals("106572018.95829985", lastIctRecord[3]);
+        assertEquals("+G:294944", lastIctRecord[4]);
+        assertEquals("106571922.30086310", lastIctRecord[5]);
+        assertEquals("+G:294944", lastIctRecord[6]);
+        assertEquals("106571971.53328174", lastIctRecord[7]);
+        assertEquals("+G:294944", lastIctRecord[8]);
+        assertEquals("106572020.76570037", lastIctRecord[9]);
+        assertEquals("+G:294944", lastIctRecord[10]);
+        assertEquals("106572069.99811901", lastIctRecord[11]);
+        assertEquals("+G:294944", lastIctRecord[12]);
+        assertEquals("106572119.23053764", lastIctRecord[13]);
+        
+        double[] ictValues = reader.getLastIctValues();
+        ImageCenterTime imageCenterTime = ImageCenterTime.create(ictValues, 13);
 
         assertNotNull(imageCenterTime);
         assertEquals(3098881.4711724636, imageCenterTime.ict1, 0.000001);
