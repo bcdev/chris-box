566a32fa9e0c2930a16c2050d77e64672625b0a1
Ralf Quast <ralf.quast@brockmann-consult.de>
2010-04-22 13:35:39 +0200

    stored calculated geometry in tie point grids again

--- chris-geometric-correction/src/main/java/org/esa/beam/chris/operators/PerformGeometricCorrectionOp.java
+++ chris-geometric-correction/src/main/java/org/esa/beam/chris/operators/PerformGeometricCorrectionOp.java
@@ -16,33 +16,30 @@
  */
 package org.esa.beam.chris.operators;
 
-import com.bc.ceres.core.ProgressMonitor;
 import org.esa.beam.chris.operators.IctDataRecord.IctDataReader;
 import org.esa.beam.chris.operators.TelemetryFinder.Telemetry;
 import org.esa.beam.chris.util.BandFilter;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.FlagCoding;
-import org.esa.beam.framework.datamodel.PixelGeoCoding;
 import org.esa.beam.framework.datamodel.Placemark;
+import org.esa.beam.framework.datamodel.PointingFactoryRegistry;
 import org.esa.beam.framework.datamodel.Product;
-import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.framework.datamodel.TiePointGeoCoding;
+import org.esa.beam.framework.datamodel.TiePointGrid;
 import org.esa.beam.framework.gpf.Operator;
 import org.esa.beam.framework.gpf.OperatorException;
 import org.esa.beam.framework.gpf.OperatorSpi;
-import org.esa.beam.framework.gpf.Tile;
 import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
 import org.esa.beam.framework.gpf.annotations.Parameter;
 import org.esa.beam.framework.gpf.annotations.SourceProduct;
 import org.esa.beam.util.ProductUtils;
 
-import java.awt.Rectangle;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Operator performing the geometric correction.
@@ -83,11 +80,6 @@ public class PerformGeometricCorrectionOp extends Operator {
     @SourceProduct(type = "CHRIS_M[012345][0A]?(_NR)?(_AC)?")
     private Product sourceProduct;
 
-    private transient IctDataRecord ictData;
-    private transient List<GpsDataRecord> gpsData;
-    private transient AcquisitionInfo info;
-    private transient GCP[] gcps;
-
     @Override
     public void initialize() throws OperatorException {
         try {
@@ -100,76 +92,27 @@ public class PerformGeometricCorrectionOp extends Operator {
             final double deltaGPS = TimeConverter.getInstance().deltaGPS(mjd);
 
             // 3. read image center times from telemetry
-            ictData = readIctData(telemetry.getIctFile(), deltaGPS);
+            final IctDataRecord ictData = readIctData(telemetry.getIctFile(), deltaGPS);
 
             // 4. read trajectory from telemetry
-            gpsData = readGpsData(telemetry.getGpsFile(), deltaGPS, DELAY);
+            final List<GpsDataRecord> gpsData = readGpsData(telemetry.getGpsFile(), deltaGPS, DELAY);
 
             // 5. create acquisition info
-            info = AcquisitionInfo.create(sourceProduct);
+            final AcquisitionInfo info = AcquisitionInfo.create(sourceProduct);
 
             // 6. create GCPs
-            gcps = GCP.createArray(sourceProduct.getGcpGroup(), info.getTargetAlt());
+            final GCP[] gcps = GCP.createArray(sourceProduct.getGcpGroup(), info.getTargetAlt());
 
-            // 7. set the target product
-            setTargetProduct(createTargetProduct());
+            // 7. create and set the target product
+            final GeometryCalculator calculator = new GeometryCalculator(ictData, gpsData, info, gcps);
+            calculator.calculate(useTargetAltitude);
+            setTargetProduct(createTargetProduct(calculator, info.isBackscanning()));
         } catch (IOException e) {
             throw new OperatorException(e);
         }
     }
 
-    @Override
-    public void dispose() {
-        ictData = null;
-        gpsData = null;
-        info = null;
-        gcps = null;
-    }
-
-    @Override
-    public void computeTileStack(Map<Band, Tile> targetTileMap, Rectangle targetRectangle,
-                                 ProgressMonitor pm) throws OperatorException {
-        try {
-            pm.beginTask("Performing geometric correction...", targetRectangle.width * targetRectangle.height);
-
-            final GeometryCalculator calculator = new GeometryCalculator(ictData, gpsData, info, gcps);
-            // todo - add progress monitor as argument(rq-20100421)
-            calculator.calculate(useTargetAltitude);
-
-            final Tile lonTile = targetTileMap.get(getTargetProduct().getBand("lon"));
-            final Tile latTile = targetTileMap.get(getTargetProduct().getBand("lat"));
-            final Tile vaaTile = targetTileMap.get(getTargetProduct().getBand("vaa"));
-            final Tile vzaTile = targetTileMap.get(getTargetProduct().getBand("vza"));
-            final Tile ipaTile;
-            final Tile iraTile;
-            if (includePitchAndRoll) {
-                ipaTile = targetTileMap.get(getTargetProduct().getBand("ipa"));
-                iraTile = targetTileMap.get(getTargetProduct().getBand("ira"));
-            } else {
-                ipaTile = null;
-                iraTile = null;
-            }
-            for (final Tile.Pos pos : lonTile) {
-                checkForCancelation(pm);
-                final int sourceRow = info.isBackscanning() ? targetRectangle.height - 1 - pos.y : pos.y;
-                lonTile.setSample(pos.x, pos.y, calculator.getLon(pos.x, sourceRow));
-                latTile.setSample(pos.x, pos.y, calculator.getLat(pos.x, sourceRow));
-                vaaTile.setSample(pos.x, pos.y, calculator.getVaa(pos.x, sourceRow));
-                vzaTile.setSample(pos.x, pos.y, calculator.getVza(pos.x, sourceRow));
-                if (includePitchAndRoll) {
-                    assert ipaTile != null;
-                    assert iraTile != null;
-                    ipaTile.setSample(pos.x, pos.y, calculator.getPitch(pos.x, sourceRow));
-                    iraTile.setSample(pos.x, pos.y, calculator.getRoll(pos.x, sourceRow));
-                }
-                pm.worked(1);
-            }
-        } finally {
-            pm.done();
-        }
-    }
-
-    private Product createTargetProduct() {
+    private Product createTargetProduct(GeometryCalculator calculator, boolean backscanning) {
         final String productType = sourceProduct.getProductType() + "_GC";
         final Product targetProduct = createCopy(sourceProduct, "GC", productType, new BandFilter() {
             @Override
@@ -178,30 +121,57 @@ public class PerformGeometricCorrectionOp extends Operator {
             }
         });
 
-        final Band lonBand = addBand(targetProduct, "lon", "Longitude (deg)", "deg");
-        final Band latBand = addBand(targetProduct, "lat", "Latitude (deg)", "deg");
-        addBand(targetProduct, "vaa", "View azimuth angle (deg)", "deg");
-        addBand(targetProduct, "vza", "View zenith angle (deg)", "deg");
-
-        if (includePitchAndRoll) {
-            addBand(targetProduct, "ipa", "Instrument pitch angle (rad)", "rad");
-            addBand(targetProduct, "ira", "Instrument roll angle (rad)", "rad");
-        }
-
         final int w = targetProduct.getSceneRasterWidth();
         final int h = targetProduct.getSceneRasterHeight();
-        targetProduct.setPreferredTileSize(w, h);
-        targetProduct.setGeoCoding(new PixelGeoCoding(latBand, lonBand, "true", 20));
+
+        final float[] lons = new float[w * h];
+        final float[] lats = new float[w * h];
+        final float[] vaas = new float[w * h];
+        final float[] vzas = new float[w * h];
+
+        for (int row = 0; row < h; row++) {
+            final int y = backscanning ? h - 1 - row : row;
+            for (int x = 0; x < w; x++) {
+                lons[row * w + x] = (float) calculator.getLon(x, y);
+                lats[row * w + x] = (float) calculator.getLat(x, y);
+                vaas[row * w + x] = (float) calculator.getVaa(x, y);
+                vzas[row * w + x] = (float) calculator.getVza(x, y);
+            }
+        }
+
+        final TiePointGrid lonGrid = addTiePointGrid(targetProduct, "lon", w, h, lons, "Longitude (deg)", "deg");
+        final TiePointGrid latGrid = addTiePointGrid(targetProduct, "lat", w, h, lats, "Latitude (deg)", "deg");
+        addTiePointGrid(targetProduct, "vaa", w, h, vaas, "View azimuth angle (deg)", "deg");
+        addTiePointGrid(targetProduct, "vza", w, h, vzas, "View zenith angle (deg)", "deg");
+
+        if (includePitchAndRoll) {
+            final float[] ipas = new float[w * h];
+            final float[] iras = new float[w * h];
+            for (int row = 0; row < h; row++) {
+                final int y = backscanning ? h - 1 - row : row;
+                for (int x = 0; x < w; x++) {
+                    ipas[row * w + x] = (float) calculator.getPitch(x, y);
+                    iras[row * w + x] = (float) calculator.getRoll(x, y);
+                }
+            }
+            addTiePointGrid(targetProduct, "ipa", w, h, ipas, "Instrument pitch angle (rad)", "rad");
+            addTiePointGrid(targetProduct, "ira", w, h, iras, "Instrument roll angle (rad)", "rad");
+        }
+
+        targetProduct.setGeoCoding(new TiePointGeoCoding(latGrid, lonGrid));
+        targetProduct.setPointingFactory(PointingFactoryRegistry.getInstance().getPointingFactory(productType));
 
         return targetProduct;
     }
 
-    private static Band addBand(Product targetProduct, String name, String description, String unit) {
-        final Band band = targetProduct.addBand(name, ProductData.TYPE_FLOAT64);
-        band.setDescription(description);
-        band.setUnit(unit);
+    private static TiePointGrid addTiePointGrid(Product targetProduct, String name, int w, int h, float[] tiePoints,
+                                                String description, String unit) {
+        final TiePointGrid lonGrid = new TiePointGrid(name, w, h, 0.5f, 0.5f, 1.0f, 1.0f, tiePoints);
+        lonGrid.setDescription(description);
+        lonGrid.setUnit(unit);
+        targetProduct.addTiePointGrid(lonGrid);
 
-        return band;
+        return lonGrid;
     }
 
     // todo - this is a quite general utility method (rq-20090708)
